58d8edfbc25426400b5c43a5042a41ebd915fc3d
jonatastrife <jonatastrife03@gmail.com>
2015-05-23 17:39:04 -0300

    Toma no cu gostoso

---
+++ ContatosDaEuropa/src/br/edu/qi/bean/AgendaBean.java
@@ -0,0 +1,17 @@
+package br.edu.qi.bean;
+
+import javax.ejb.Local;
+import javax.ejb.Stateless;
+
+import br.edu.qi.dao.AgendaDao;
+import br.edu.qi.dto.Agenda;
+
+@Stateless
+@Local
+public class AgendaBean {
+
+	public void carregar() throws Exception
+	{
+		
+	}
+}
