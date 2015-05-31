6596eba51fd9398ea66abb567d8f5a0d71d41526
jonatastrife <jonatastrife03@gmail.com>
2015-05-31 17:49:53 -0300

    Fiz essa merda funcionar, XUPA!!

--- ContatosDaEuropa/src/br/edu/qi/mb/ListarContatosMB.java
+++ ContatosDaEuropa/src/br/edu/qi/mb/ListarContatosMB.java
@@ -6,36 +6,20 @@ import java.util.ArrayList;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.faces.context.FacesContext;
-import javax.faces.model.DataModel;
-import javax.faces.model.ListDataModel;
 
 import br.edu.qi.bean.ContatoBean;
 import br.edu.qi.model.Contato;
 
-@ManagedBean(name="listaContato")
+@ManagedBean(name = "listaContato")
 @SessionScoped
-public class ListarContatosMB implements Serializable{
+public class ListarContatosMB implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	@EJB
 	ContatoBean bean;
-	
+
 	private ArrayList<Contato> contatos;
-//	private static final ArrayList<Contato> contatos= new ArrayList<Contato>(Arrays.asList(
-//			new Contato("Dionatan", "Dio", "shjds", "sdfs", "sjka", new Date())));;
-	
-
-	private Contato contatoSelecionado;
-	
-	public Contato getContatoSelecionado() {
-		return contatoSelecionado;
-	}
-
-	public void setContatoSelecionado(Contato contatoSelecionado) {
-		this.contatoSelecionado = contatoSelecionado;
-	}
 
 	public ArrayList<Contato> getContatos() {
 		if (contatos == null) {
@@ -58,43 +42,20 @@ public class ListarContatosMB implements Serializable{
 		}
 		return null;
 	}
-	
+
 	public String atualizarContato(Contato contato) {
 		contato.setEditable(true);
 		return null;
 	}
-	
-	private transient DataModel<Contato> model;
-	
-	public DataModel<Contato> getModel() {
-	    if (model == null) {
-	    	try {
-				model = new ListDataModel<Contato>(bean.getContatos());
-			} catch (Exception e) {
-				MBUtils.buildMessage("mensagemAlerta", e.getMessage());
-			}
-	    }
-	    return model;
-	}
-	
+
 	public String salvarAlteracoes(Contato contato) {
-		getContatoSelecionado();
-		FacesContext context = FacesContext.getCurrentInstance();
-	    Contato outro = context.getApplication().evaluateExpressionGet(context, "#{contato}", Contato.class);
-		Contato novo = model.getRowData();
-		
-		for (Contato c : contatos){
-			if (c.isEditable()) {
-				try {
-					bean.update(c);
-				} catch (Exception e) {
-					MBUtils.buildMessage("mensagemAlerta", e.getMessage());
-				}
-				c.setEditable(false);
-			}
+		try {
+			bean.update(contato);
+		} catch (Exception e) {
+			MBUtils.buildMessage("mensagemAlerta", e.getMessage());
 		}
 		contatos = null;
 		return null;
-	}	
-	
+	}
+
 }
